6f212cf07864bde7a82537407b08599f33a1af5d
Klearchos-K <32612550+Klearchos-K@users.noreply.github.com>
2020-10-07 10:22:20 +0300

    Update api.py

--- api.py
+++ api.py
@@ -11,22 +11,8 @@ print(k)
 r = sys.argv[4]
 print("r= ", r)
 foo = Graph_l()
-foo.load(filename, "	", "#")
-#foo.compute_cores(foo.Gr)
-
+#foo.load(filename, "	", "#")
+foo.load(filename, " ", "#")
 foo.expreriments(int(q), int(k), int(r))
-exit(0)
 
-def thr():
-    print('me kalesan')
-    foo = Graph_l()
-    foo.load("youtube2.txt", " ", "#")
-    foo.expreriments(100, 100, r)
-
-
-
-for i in range(10):
-    #thread = threading.Thread(target=thr())
-    #thread.start()
-    thr()
 
